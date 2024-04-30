e1abd40189b254481cd2ebfccc5dbb5c47972502
Denisoed <denisoed@gmal.com>
2024-04-30 17:07:15 +0600

    Updated readme & added new CreateGoal contract

--- tests/CreateGoal.spec.ts
+++ tests/CreateGoal.spec.ts
@@ -4,38 +4,38 @@ import { CreateGoal } from '../wrappers/CreateGoal';
 import '@ton/test-utils';
 
 describe('CreateGoal', () => {
-    let blockchain: Blockchain;
-    let deployer: SandboxContract<TreasuryContract>;
-    let createGoal: SandboxContract<CreateGoal>;
+  let blockchain: Blockchain;
+  let deployer: SandboxContract<TreasuryContract>;
+  let createGoal: SandboxContract<CreateGoal>;
 
-    beforeEach(async () => {
-        blockchain = await Blockchain.create();
+  beforeEach(async () => {
+    blockchain = await Blockchain.create();
 
-        createGoal = blockchain.openContract(await CreateGoal.fromInit());
+    createGoal = blockchain.openContract(await CreateGoal.fromInit());
 
-        deployer = await blockchain.treasury('deployer');
+    deployer = await blockchain.treasury('deployer');
 
-        const deployResult = await createGoal.send(
-            deployer.getSender(),
-            {
-                value: toNano('0.05'),
-            },
-            {
-                $$type: 'Deploy',
-                queryId: 0n,
-            }
-        );
+    const deployResult = await createGoal.send(
+      deployer.getSender(),
+      {
+        value: toNano('0.05'),
+      },
+      {
+        $$type: 'Deploy',
+        queryId: 0n,
+      },
+    );
 
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: createGoal.address,
-            deploy: true,
-            success: true,
-        });
+    expect(deployResult.transactions).toHaveTransaction({
+      from: deployer.address,
+      to: createGoal.address,
+      deploy: true,
+      success: true,
     });
+  });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and createGoal are ready to use
-    });
+  it('should deploy', async () => {
+    // the check is done inside beforeEach
+    // blockchain and createGoal are ready to use
+  });
 });
